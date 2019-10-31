68ef06c011488f3e142cfc3f0ebf3b0d74d7da61
Jan-Oliver Jahner <jojahner@gmail.com>
2019-10-31 01:47:02 +0100

    Add simple simulation

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,42 @@
 mod tuple;
 
+#[derive(Copy, Clone, PartialEq, Debug)]
+struct Projectile {
+    position: tuple::Point,
+    velocity: tuple::Vector
+}
+
+#[derive(Copy, Clone, PartialEq, Debug)]
+struct Environment {
+    gravity: tuple::Vector,
+    wind: tuple::Vector
+}
+
+fn tick(projectile: Projectile, environment: Environment) -> Projectile {
+    let position = projectile.position + projectile.velocity;
+    let velocity = projectile.velocity + environment.gravity + environment.wind;
+
+    Projectile { position, velocity }
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let mut projectile = Projectile {
+        position: tuple::Point { x: 0.0, y: 1.0, z: 0.0 },
+        velocity: tuple::Vector { x: 0.9, y: 0.75, z: 0.0 }.normalize()
+    };
+
+    let environment = Environment {
+        gravity: tuple::Vector { x: 0.0, y: -0.1, z: 0.0 },
+        wind: tuple::Vector { x: -0.01, y: 0.0, z: 0.0 },
+    };
+
+    loop {
+        println!("{:?}, {:?}", projectile.position.x, projectile.position.y);
+        projectile = tick(projectile, environment);
+
+        if projectile.position.y < 0.0 {
+            break;
+        }
+    }
 }
